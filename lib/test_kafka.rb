283d7c1c1996d574bf644039e3e09a55176b6aa9
Brian Schroeder <bts@gmail.com>
2013-11-09 13:48:20 -0800

    Make it harder to wipe directories like /tmp

--- lib/test_kafka.rb
+++ lib/test_kafka.rb
@@ -2,7 +2,8 @@ require 'fileutils'
 require 'test_kafka/cluster'
 
 module TestKafka
-  def self.start(kafka_path, tmp_dir='/tmp/kafka', kafka_port=9092, zk_port=2181)
+  def self.start(kafka_path, tmp_root='/tmp', kafka_port=9092, zk_port=2181)
+    tmp_dir = tmp_root + "/test_kafka"
     FileUtils.rm_rf(tmp_dir)
 
     TestKafka::Cluster.new(kafka_path, tmp_dir, kafka_port, zk_port).start
