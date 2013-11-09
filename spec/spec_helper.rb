8fd753cb2588dc08ca6e1292e1a6599defd02c78
Brian Schroeder <bts@gmail.com>
2013-11-09 14:50:15 -0800

    Fail if Kafka can't be found

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,12 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
 end
+
+DEFAULT_KAFKA_PATH = "/usr/local/kafka"
+KAFKA_PATH = ENV["KAFKA_PATH"] || DEFAULT_KAFKA_PATH
+
+require 'test_kafka/java_runner'
+
+if Dir.glob(KAFKA_PATH + "/" + TestKafka::JavaRunner::JAR_PATTERN).empty?
+  fail "Could not find Kafka. Set the environment variable KAFKA_PATH or install Kafka to /usr/local/kafka."
+end
