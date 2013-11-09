9048afb3d0cecbda79c16306b18bdb3106fcf02f
Brian Schroeder <bts@gmail.com>
2013-11-09 13:00:54 -0800

    Use /tmp/kafka instead of /tmp

--- lib/test_kafka.rb
+++ lib/test_kafka.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 require 'test_kafka/cluster_runner'
 
 module TestKafka
-  def self.start(kafka_path, tmp_dir='/tmp', kafka_port=9092, zk_port=2181)
+  def self.start(kafka_path, tmp_dir='/tmp/kafka', kafka_port=9092, zk_port=2181)
     FileUtils.rm_rf(tmp_dir)
 
     TestKafka::ClusterRunner.new(kafka_path, tmp_dir, kafka_port, zk_port).start
